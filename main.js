7b075699d282fb064a4508d7263d9be89dbd21d4
quantum0X <thevishal2108@gmail.com>
2023-08-08 02:39:12 +0530

    form connected to database

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import ejs from 'ejs'
+import { connect, model, Schema } from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
 const port = 3000
@@ -7,10 +9,36 @@ const port = 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+dotenv.config()
+
 app.set('view engine', 'ejs')
+
+const path = process.env.MONGODB_URL
+
+connect(path)
+
+const userSchema = new Schema({
+    usermail: 'String',
+    password: 'String'
+})
+
+const userAuth = new model('authUser', userSchema)
 
 app.get('/', (req, res) => {
     res.render('signup')
+})
+
+app.post('/register', async (req, res) => {
+    console.log(req.body)
+
+    const newUser = userAuth({
+        usermail: req.body.email,
+        password: req.body.password
+    })
+
+    await newUser.save()
+    console.log("saved succesfully")
+    res.redirect('/')
 })
 
 app.listen(port, () => {
